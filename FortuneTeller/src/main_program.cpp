2160ece163b4768ea1baecd0c7a4adbdea420831
Bonnie Bell <cj6bonnie@gmail.com>
2020-12-09 20:41:38 -0700

    fortune teller stub

---
+++ FortuneTeller/src/main_program.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fortune Teller
+ *  *
+ *  Created on: Nov 9, 2020
+ *      Author: cj6bo
+ */
+
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+	std::vector<std::string> fortunes;
+
+	return (0);
+}
